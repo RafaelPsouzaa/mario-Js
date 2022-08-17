bb056ec50e22e5e3c15b0795736c06aff9240f8e
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-08-17 15:35:09 -0300

    adding Mario jump event

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-//https://www.youtube.com/watch?v=r9buAwVBDhA&t=3s&ab_channel=ManualdoDev    tempo do video02:03
+//https://www.youtube.com/watch?v=r9buAwVBDhA&t=3s&ab_channel=ManualdoDev    tempo do video 11:40
+const mario = document.querySelector('.mario');
+
+const jumpMario = ()=> {
+    mario.classList.add('jumpMario');
+    setTimeout(()=>{
+        mario.classList.remove('jumpMario');
+    },500)
+}
+
+document.addEventListener('keydown',jumpMario);
+
+
