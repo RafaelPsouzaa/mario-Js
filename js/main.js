cf54454c2357fab9e0ed48f297d1e67bffae8513
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-08-18 11:56:14 -0300

    jump mario

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//https://www.youtube.com/watch?v=r9buAwVBDhA&t=3s&ab_channel=ManualdoDev    tempo do video 17:25 nao reconhece offsetleft
+//https://www.youtube.com/watch?v=r9buAwVBDhA&t=3s&ab_channel=ManualdoDev    tempo do video 25:04 nao reconhece offsetleft
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 
@@ -11,8 +11,10 @@ const jumpMario = ()=> {
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    console.log(marioPosition);
     
-    if(pipePosition <= 65){
+    if(pipePosition <= 65 && marioPosition < 68){    
         pipe.style.animation ='nome';
         pipe.style.left = `${pipePosition}px`
     }
